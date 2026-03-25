cc55f2b6b0579ced376c77b8b3b46873299a9997
VanTran <vantran15122004@gmail.com>
2026-03-25 17:54:06 +0700

    complete the network flow monitoring configuration for Proxmox VMs (v1)

--- traffic-network/pmacct-monitoring/sql-scripts/01-init-tables.sql
+++ traffic-network/pmacct-monitoring/sql-scripts/01-init-tables.sql
@@ -1,6 +1,5 @@
 CREATE TABLE acct_v4 (
 agent_id INT NOT NULL DEFAULT 0, 
-tag BIGINT NOT NULL DEFAULT 0,
 ip_src INET NOT NULL DEFAULT '0.0.0.0',
  	ip_dst INET NOT NULL DEFAULT '0.0.0.0',
 ip_proto SMALLINT NOT NULL DEFAULT 0,
@@ -10,7 +9,7 @@ stamp_inserted TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
 stamp_updated TIMESTAMP WITHOUT TIME ZONE,
 country_ip_src CHAR(2) NOT NULL DEFAULT '--',
 country_ip_dst CHAR(2) NOT NULL DEFAULT '--',
-PRIMARY KEY (agent_id, tag, ip_src, ip_dst, ip_proto, stamp_inserted)  
+PRIMARY KEY (agent_id, ip_src, ip_dst, ip_proto, stamp_inserted)  
 );
 
 GRANT ALL PRIVILEGES ON TABLE acct_v4 TO pmacct_user;
